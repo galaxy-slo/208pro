3b3ee50e273315200b8e1f6e7b222e8f04517039
gooping344 <cheekr2@mailbox.winthrop.edu>
2026-09-21 22:33:57 -0400

    HELPER

--- Book.cc
+++ Book.cc
@@ -18,6 +18,6 @@ void Book::SetYear(string InYear){
 }
 
 void Book::PrintBooks(){
-    cout << author << ", " << "\"" << title << "\"" << "(" << year << ")" << endl;
+    cout << author << ", " << "\"" << title << "\" (" << year << ")" << endl;
 
 }
